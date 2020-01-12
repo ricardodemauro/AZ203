eebbe275d857da5650bfd800172fc18338c20b66
Ricardo Mauro <ricardo.demauro@gmail.com>
2020-01-12 19:27:28 -0300

    adding demo using device code with console app

---
+++ AZ-203T04A-Identity/Console-DeviceCode/daemon-console/Program.cs
@@ -0,0 +1,210 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates; //Only import this if you are using certificate
+using System.Threading.Tasks;
+
+namespace daemon_console
+{
+    /// <summary>
+    /// This sample shows how to query the Microsoft Graph from a daemon application
+    /// which uses application permissions.
+    /// For more information see https://aka.ms/msal-net-client-credentials
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile();
+
+            // You can run this sample using ClientSecret or Certificate. The code will differ only when instantiating the IConfidentialClientApplication
+            bool isUsingClientSecret = AppUsesClientSecret(config);
+
+
+            IPublicClientApplication app = PublicClientApplicationBuilder
+                .Create(config.ClientId)
+                .WithAuthority(config.Authority)
+                .WithDefaultRedirectUri()
+                .Build();
+
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+            AuthenticationResult result = null;
+            var accounts = await app.GetAccountsAsync();
+
+            result = await AcquireByDeviceCodeAsync(app, scopes);
+
+            if (result != null)
+            {
+                var httpClient = new HttpClient();
+                var apiCaller = new ProtectedApiCallHelper(httpClient);
+                await apiCaller.CallWebApiAndProcessResultASync("https://graph.microsoft.com/v1.0/users", result.AccessToken, Display);
+            }
+        }
+
+        private static async Task<AuthenticationResult> AcquireByDeviceCodeAsync(IPublicClientApplication pca, string[] scopes)
+        {
+            try
+            {
+                var result = await pca.AcquireTokenWithDeviceCode(scopes,
+                    deviceCodeResult =>
+                    {
+                        // This will print the message on the console which tells the user where to go sign-in using 
+                        // a separate browser and the code to enter once they sign in.
+                        // The AcquireTokenWithDeviceCode() method will poll the server after firing this
+                        // device code callback to look for the successful login of the user via that browser.
+                        // This background polling (whose interval and timeout data is also provided as fields in the 
+                        // deviceCodeCallback class) will occur until:
+                        // * The user has successfully logged in via browser and entered the proper code
+                        // * The timeout specified by the server for the lifetime of this code (typically ~15 minutes) has been reached
+                        // * The developing application calls the Cancel() method on a CancellationToken sent into the method.
+                        //   If this occurs, an OperationCanceledException will be thrown (see catch below for more details).
+                        Console.WriteLine(deviceCodeResult.Message);
+                        return Task.FromResult(0);
+                    }).ExecuteAsync();
+
+                Console.WriteLine(result.Account.Username);
+                return result;
+            }
+            // TODO: handle or throw all these exceptions
+            catch (MsalServiceException ex)
+            {
+                // Kind of errors you could have (in ex.Message)
+
+                // AADSTS50059: No tenant-identifying information found in either the request or implied by any provided credentials.
+                // Mitigation: as explained in the message from Azure AD, the authoriy needs to be tenanted. you have probably created
+                // your public client application with the following authorities:
+                // https://login.microsoftonline.com/common or https://login.microsoftonline.com/organizations
+
+                // AADSTS90133: Device Code flow is not supported under /common or /consumers endpoint.
+                // Mitigation: as explained in the message from Azure AD, the authority needs to be tenanted
+
+                // AADSTS90002: Tenant <tenantId or domain you used in the authority> not found. This may happen if there are 
+                // no active subscriptions for the tenant. Check with your subscription administrator.
+                // Mitigation: if you have an active subscription for the tenant this might be that you have a typo in the 
+                // tenantId (GUID) or tenant domain name.
+            }
+            catch (OperationCanceledException ex)
+            {
+                // If you use a CancellationToken, and call the Cancel() method on it, then this *may* be triggered
+                // to indicate that the operation was cancelled. 
+                // See https://docs.microsoft.com/en-us/dotnet/standard/threading/cancellation-in-managed-threads 
+                // for more detailed information on how C# supports cancellation in managed threads.
+            }
+            catch (MsalClientException ex)
+            {
+                // Possible cause - verification code expired before contacting the server
+                // This exception will occur if the user does not manage to sign-in before a time out (15 mins) and the
+                // call to `AcquireTokenWithDeviceCode` is not cancelled in between
+            }
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Display the result of the Web API call
+        /// </summary>
+        /// <param name="result">Object to display</param>
+        private static void Display(JObject result)
+        {
+            foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            {
+                Console.WriteLine($"{child.Name} = {child.Value}");
+            }
+        }
+
+        /// <summary>
+        /// Checks if the sample is configured for using ClientSecret or Certificate. This method is just for the sake of this sample.
+        /// You won't need this verification in your production application since you will be authenticating in AAD using one mechanism only.
+        /// </summary>
+        /// <param name="config">Configuration from appsettings.json</param>
+        /// <returns></returns>
+        private static bool AppUsesClientSecret(AuthenticationConfig config)
+        {
+            if (!string.IsNullOrWhiteSpace(config.ClientSecret))
+            {
+                return true;
+            }
+
+            else if (!string.IsNullOrWhiteSpace(config.CertificateName))
+            {
+                return false;
+            }
+
+            else
+                throw new Exception("You must choose between using client secret or certificate. Please update appsettings.json file.");
+        }
+
+        private static X509Certificate2 ReadCertificate(string certificateName)
+        {
+            if (string.IsNullOrWhiteSpace(certificateName))
+            {
+                throw new ArgumentException("certificateName should not be empty. Please set the CertificateName setting in the appsettings.json", "certificateName");
+            }
+            X509Certificate2 cert = null;
+
+            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certCollection = store.Certificates;
+
+                // Find unexpired certificates.
+                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+
+                // From the collection of unexpired certificates, find the ones with the correct name.
+                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certificateName, false);
+
+                // Return the first certificate in the collection, has the right name and is current.
+                cert = signingCert.OfType<X509Certificate2>().OrderByDescending(c => c.NotBefore).FirstOrDefault();
+            }
+            return cert;
+        }
+
+    }
+}
