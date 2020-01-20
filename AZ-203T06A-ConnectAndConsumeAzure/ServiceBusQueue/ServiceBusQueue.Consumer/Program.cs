a3136e4d20a44f589c9ac7ef2359f32075d1cb24
Ricardo Mauro <ricardo.demauro@gmail.com>
2020-01-19 21:50:06 -0300

    adding service bus lab

---
+++ AZ-203T06A-ConnectAndConsumeAzure/ServiceBusQueue/ServiceBusQueue.Consumer/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Based on article
+/// https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues
+/// </summary>
+
+namespace ServiceBusQueue.Consumer
+{
+    class Program
+    {
+        private static readonly IConfiguration Configuration;
+
+        static Program()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddUserSecrets("f134261b-1f56-4609-abf5-fc0497a5cb88");
+
+            Configuration = builder.Build();
+        }
+
+        static async Task Main(string[] args)
+        {
+            string connectionString = Configuration["bus_connection_string"];
+            string queueName = Configuration["bus_queue_name"];
+
+            IQueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Register the queue message handler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages(queueClient);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages(IQueueClient queueClient)
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler((msg, cancellationToken) => ProcessMessagesAsync(queueClient, msg, cancellationToken), messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(IQueueClient queueClient, Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AZ-203T06A-ConnectAndConsumeAzure/ServiceBusQueue/ServiceBusQueue.Publisher/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Based on article
+/// https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-dotnet-get-started-with-queues
+/// </summary>
+
+namespace ServiceBusQueue.Publisher
+{
+    class Program
+    {
+        private static readonly IConfiguration Configuration;
+
+        static Program()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddUserSecrets("3b22e8cd-85dc-441e-9767-c8126a89d051");
+
+            Configuration = builder.Build();
+        }
+
+        static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+
+            string connectionString = Configuration["bus_connection_string"];
+            string queueName = Configuration["bus_queue_name"];
+
+            IQueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(queueClient, numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(IQueueClient queueClient, int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
